9917450a9e46ec1e2ede477b0a2848a8fee8e8ae
Yen Ter <leeyenter@gmail.com>
2019-03-16 18:07:17 +0800

    City stuff

--- city_script.js
+++ city_script.js
@@ -5,6 +5,15 @@ var app = new Vue({
       info: {}, 
       actions: [],
       city_name: 'Singapore'
+    }, 
+    created: function() {
+      fetch("https://ync.yenter.io/get_info/" + this.city_name).then((resp) => {
+        return resp.json()
+      }).then((resp) => {
+        console.log(resp)
+        this.info = resp;
+        console.log(resp['actions'])
+      })
     }
   })
   
